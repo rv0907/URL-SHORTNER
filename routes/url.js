0cd0a7c749e4e6b58d562aad9066bfb6d920250c
RUDRAVEER SINGH <136282608+rv0907@users.noreply.github.com>
2025-08-21 00:46:31 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { nanoid } = require("nanoid");
-const Url = require("../models/url");
+const Url = require("../models/Url");
 
 // Create Short URL
 router.post("/shorten", async (req, res) => {
