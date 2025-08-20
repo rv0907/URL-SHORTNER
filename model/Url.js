2f3ba3f64addd89afb0aaa2e38fcc8d1a802ed54
RUDRAVEER SINGH <136282608+rv0907@users.noreply.github.com>
2025-08-21 00:59:54 +0530

    Update Url.js

--- model/Url.js
+++ model/Url.js
@@ -1,4 +1,14 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+
+// use process.env.MONGO_URI
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(" Connected to MongoDB Atlas"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
 const urlSchema = new mongoose.Schema(
   {
